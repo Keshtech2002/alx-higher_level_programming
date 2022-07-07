3ac4932c26a4b99d2496b75586ecd738a21e3211
Keshtech2002 <keshtech2002@gmail.com>
2022-07-06 17:30:11 -0700

    square matrix using map()

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    new_matrix = matrix.copy()
-    return list(map(lambda row: list(map(lambda x: x * x, row)), new_matrix))
+    return list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
