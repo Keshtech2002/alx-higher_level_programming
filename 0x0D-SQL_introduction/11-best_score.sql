6ad2839871436e5c1f1e5f562703040b484419b5
Keshtech2002 <keshtech2002@gmail.com>
2022-09-07 12:55:54 -0700

    lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- List all records with score >= 10 in the second_table of hbtn_0c_0
+-- display score and name and order by score DESC
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10
+ORDER BY score DESC;
