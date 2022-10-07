4420d082e25d38be20b1928c5909dfe31f099618
Keshtech2002 <keshtech2002@gmail.com>
2022-10-07 05:56:37 +0000

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,23 +1,11 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-
-const occurrences = [];
-for (const key in dict) {
-  if (occurrences.includes(dict[key])) {
-    continue;
-  }
-  occurrences.push(dict[key]);
-}
-
+const dict = require('./101-data.js').dict;
 const newDict = {};
-
-for (const occ of occurrences) {
-  const ids = [];
-  for (const [key, value] of Object.entries(dict)) {
-    if (occ === value) {
-      ids.push(key);
-    }
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-  newDict[occ] = ids;
 }
 console.log(newDict);
