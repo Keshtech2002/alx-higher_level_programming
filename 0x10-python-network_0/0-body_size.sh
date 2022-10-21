fd5e67069feb7e9d33efdd37a164094ade75f04a
Keshtech2002 <keshtech2002@gmail.com>
2022-10-20 21:18:51 -0700

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # Bash script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 # display number of bytes in location
-curl -s "$1" | wc -c
+curl -sI $1 | grep -oP 'Content-Length: \K\d+'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,4 +2,5 @@
 # Bash script that takes in a URL, sends a GET request to the URL,
 # and displays the body of the response
 # takes in URL and redirects to new location
-curl -sL "$1"
+# prints the body of a response only if status is 200
+if [ "$(curl -sL -o /dev/null -sw '%{http_code}' "$1")" -eq 200 ]; then curl -sL "$1"; fi
